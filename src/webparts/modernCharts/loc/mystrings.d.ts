3fe03fa00ecfc703517ce280aed1c49640d7f4c1
DRON-10 <55283731+DRON-10@users.noreply.github.com>
2020-08-07 20:36:51 +0530

    Initial commit

---
+++ src/webparts/modernCharts/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IModernChartsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ModernChartsWebPartStrings' {
+  const strings: IModernChartsWebPartStrings;
+  export = strings;
+}
